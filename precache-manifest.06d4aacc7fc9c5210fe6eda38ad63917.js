95b20746a09d9ce671654c1c98cfe1643c19da90
Thierno Amadou Diallo <thiernoadi@gmail.com>
2020-10-01 00:51:30 -0400

    Updates

---
+++ precache-manifest.06d4aacc7fc9c5210fe6eda38ad63917.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fd24bd0ef546d0024810",
+    "url": "/amisrobo/static/css/main.28094778.chunk.css"
+  },
+  {
+    "revision": "fd24bd0ef546d0024810",
+    "url": "/amisrobo/static/js/main.fd24bd0e.chunk.js"
+  },
+  {
+    "revision": "7619cb3a498227997d08",
+    "url": "/amisrobo/static/css/1.408aec90.chunk.css"
+  },
+  {
+    "revision": "7619cb3a498227997d08",
+    "url": "/amisrobo/static/js/1.7619cb3a.chunk.js"
+  },
+  {
+    "revision": "e3393eb32784c8215e34",
+    "url": "/amisrobo/static/js/runtime~main.e3393eb3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/amisrobo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9f0b58ea06487ed520885113a7cb0eb1",
+    "url": "/amisrobo/index.html"
+  }
+];
